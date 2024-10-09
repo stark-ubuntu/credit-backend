267d29776b2e63e3173513362ece2291519b5e91
Matheus Fideles <matheuss.fideles@hotmail.com>
2024-10-09 20:33:35 -0300

    create SaveIntentionTransferServices to used @Transactional

---
+++ src/main/java/com/br/challenge/ubuntu/services/SaveIntentionTransferServices.java
@@ -0,0 +1,17 @@
+package com.br.challenge.ubuntu.services;
+
+import com.br.challenge.ubuntu.entities.Transfer;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+
+import java.io.Serializable;
+
+@ApplicationScoped
+public class SaveIntentionTransferServices implements Serializable {
+
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    public void execute(Transfer transfer) {
+        transfer.persistAndFlush();
+        System.out.println("transfer intention registered");
+    }
+}
