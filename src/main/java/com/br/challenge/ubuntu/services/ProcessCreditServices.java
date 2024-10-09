4df8dcbecb0bfa2c9d7868108abd3d4ff947a65a
Matheus Fideles <matheuss.fideles@hotmail.com>
2024-10-09 20:32:40 -0300

    update ProcessCreditServices

--- src/main/java/com/br/challenge/ubuntu/services/ProcessCreditServices.java
+++ src/main/java/com/br/challenge/ubuntu/services/ProcessCreditServices.java
@@ -5,27 +5,28 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.starkbank.Invoice;
 import io.quarkus.vertx.ConsumeEvent;
+import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
-import org.jetbrains.annotations.Blocking;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.Map;
 
 @ApplicationScoped
-public class ProcessCredit {
+public class ProcessCreditServices {
 
     @Inject
-    @Channel("challenge") // O nome do canal para enviar as mensagens
+    @Channel("challenge")
     Emitter<String> emitter;
+
+    @Inject
+    SaveIntentionTransferServices saveIntentionTransferServices;
 
     @ConsumeEvent(value = "process-credit")
     @Blocking
-    public void consume(Invoice.Log log) {
+    public void execute(Invoice.Log log) {
         BigDecimal amount = BigDecimal.valueOf(log.invoice.amount.intValue());
         BigDecimal interestAmount = BigDecimal.valueOf(log.invoice.interestAmount.intValue());
         BigDecimal fine = BigDecimal.valueOf(log.invoice.fine.doubleValue());
@@ -39,20 +40,23 @@ public class ProcessCredit {
                 .add(interest)
                 .add(interestAmount);
 
-//        Transfer transfer = new Transfer();
-//        transfer.setAmount(transferAmount);
-//        transfer.setTaxId(log.invoice.taxId);
-//        transfer.setStatus("Pending");
-//        transfer.persist();
-
         Gson gson = new Gson();
         HashMap<String, JsonObject> credit = new HashMap<>();
         JsonObject jsonObject = new JsonObject();
+
         jsonObject.addProperty("amount", transferAmount);
         jsonObject.addProperty("outTaxId", log.invoice.taxId);
         credit.put(log.invoice.taxId, jsonObject);
         String json = gson.toJson(credit);
+
         emitter.send(json);
+
+        Transfer transfer = new Transfer();
+        transfer.setAmount(transferAmount.intValue());
+        transfer.setTaxId(log.invoice.taxId);
+        transfer.setStatus("Pending");
+
+        saveIntentionTransferServices.execute(transfer);
     }
 
 
